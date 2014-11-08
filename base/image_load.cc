043b42873dd86a8453d5f698cb079c72f7bf3066
lenxyang <lenxyang@gmail.com>
2014-11-08 20:14:41 +0800

    .

---
+++ base/image_load.cc
@@ -0,0 +1,13 @@
+#include "azer/base/image.h"
+
+#include "azer/base/image_data.h"
+
+namespace azer {
+ImageData* ImageData::Load(const ::base::FilePath& path) {
+  return NULL;
+}
+
+Image* Image::Load(const ::base::FilePath& path) {
+  return NULL;
+}
+}  // namespace azer
