5f5678250e600b39a15d4aa092c07762cc8e577a
lenxyang <lenxyang@gmail.com>
2014-12-03 15:27:52 +0800

    .

--- math/vector2.h
+++ math/vector2.h
@@ -12,6 +12,13 @@ class Vector2 {
   Vector2 operator * (float v) const;
   Vector2 operator + (const Vector2 &v) const;
   Vector2 operator - (const Vector2 &v) const;
+  Vector2& operator = (const Vector2& v);
+
+  float dot(const Vector2& v) const;
+
+  float length() const;
+  Vector2& Normalize();
+  Vector2 NormalizeCopy() const;
  public:
   float x, y;
 };
@@ -21,8 +28,18 @@ inline Vector2::Vector2(float p1, float p2) : x(p1), y(p2) {}
 inline Vector2::Vector2(float arr[2]) : x(*arr), y(*(arr + 1)) {}
 inline Vector2::Vector2(const Vector2& v) : x(v.x), y(v.y) {}
 
+inline Vector2& Vector2::operator = (const Vector2& v) {
+  this->x = v.x;
+  this->y = v.y;
+  return *this;
+}
+
 inline Vector2 Vector2::operator * (float v) const {
   return Vector2(x * v, y * v);
+}
+
+inline float Vector2::dot(const Vector2& v) const {
+  return x * v.x + y * v.y;
 }
 
 inline Vector2 Vector2::operator + (const Vector2 &v) const {
@@ -31,5 +48,22 @@ inline Vector2 Vector2::operator + (const Vector2 &v) const {
 
 inline Vector2 Vector2::operator - (const Vector2 &v) const {
   return Vector2(x - v.x, y - v.y);
+}
+
+inline float Vector2::length() const {
+  return std::sqrt(x * x + y * y);
+}
+
+inline Vector2& Vector2::Normalize() {
+  float len = length();
+  x /= len;
+  y /= len;
+  return *this;
+}
+
+inline Vector2 Vector2::NormalizeCopy() const {
+  Vector2 v(x, y);
+  v.Normalize();
+  return v;
 }
 }  // namespace azer
