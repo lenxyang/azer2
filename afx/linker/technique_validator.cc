3dae0e1869927c3ac4d24c188e6985d05ae3be8e
yanglei <lenxyang@gmail.com>
2014-12-14 11:51:35 +0800

    add TechniqueValidator

--- afx/linker/technique_validator.cc
+++ afx/linker/technique_validator.cc
@@ -1,9 +1,23 @@
 #include "azer/afx/linker/technique_validator.h"
 
+#include "base/logging.h"
+#include "azer/afx/compiler/astnode.h"
+#include "azer/afx/compiler/technique_parser.h"
+#include "azer/render/render_system_enum.h"
+
 namespace azer {
 namespace afx {
 
 bool TechniqueValidator::Valid(const Technique& technique) {
+  if (technique.shader[kGeometryStage].entry != NULL) {
+    if (!ValidGeometryShader(technique.shader[kGeometryStage])) {
+      return false;
+    }
+  }
+  return true;
+}
+
+bool TechniqueValidator::ValidGeometryShader(const Technique::ShaderInfo& shader) {
   return true;
 }
 }  // namespace afx
--- afx/linker/technique_validator.h
+++ afx/linker/technique_validator.h
@@ -1,14 +1,24 @@
 #pragma once
 
 #include "base/basictypes.h"
-#include "azer/afx/linker/technique_parser.h"
+#include "azer/afx/linker/technique.h"
 
 namespace azer {
 namespace afx {
+
+/**
+ * class TechniqueValidator
+ * 用于检测 Technique 是否有不合法的地方
+ * 具体检测规则包括
+ * 1. geometry shader
+ *   gs 是否指定了最大的顶点数量
+ *   gs 的 entry 函数的格式是否符合标准
+ **/
 class TechniqueValidator {
  public:
   bool Valid(const Technique& technique);
  private:
+  bool ValidGeometryShader(const Technique::ShaderInfo& shader);
   DISALLOW_COPY_AND_ASSIGN(TechniqueValidator);
 };
 }  // namespace afx
