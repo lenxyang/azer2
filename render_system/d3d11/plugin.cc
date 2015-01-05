e3a2b1319fb3700fac4976d657517a64fbb617be
lenxyang <lenxyang@gmail.com>
2015-01-05 14:14:56 +0800

    refactor d3denv

--- render_system/d3d11/plugin.cc
+++ render_system/d3d11/plugin.cc
@@ -1,14 +1,16 @@
 #include "azer/render_system/d3d11/plugin.h"
 #include "azer/render_system/d3d11/d3denv.h"
 #include "azer/render_system/d3d11/render_system.h"
+#include "azer/render_system/d3d11/d3d_internal_env.h"
 
 using azer::d3d11::D3DEnvironment;
 using azer::d3d11::D3DEnvironmentPtr;
 using azer::d3d11::D3DRenderSystem;
+using azer::d3d11::InternalD3DEnvironment;
 
 extern "C" {
 azer::RenderSystem* CreateRenderSystem(azer::Surface* sur) {
-  D3DEnvironmentPtr envptr = D3DEnvironment::Create("internal", sur);
+  D3DEnvironmentPtr envptr(new InternalD3DEnvironment(surface));
   if (envptr.get() == NULL) {
     LOG(ERROR) << "Failed to inititialize D3D11Environment";
     return NULL;
