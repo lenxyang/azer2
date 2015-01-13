3da8d0ea21992257b1157a17d60c431bc41bb8f3
lenxyang <lenxyang@gmail.com>
2015-01-13 18:19:18 +0800

    .

--- ui/win/window_delegate.h
+++ ui/win/window_delegate.h
@@ -1,6 +1,27 @@
 #pragma once
 
+#include "azer/base/export.h"
+
+namespace gfx {
+class Canvas;
+class Path;
+class Point;
+class Rect;
+class Size;
+}  // namespace gfx
+
 namespace azer {
 namespace win {
+class AZER_EXPORT WindowDelegate {
+public:
+  virtual gfx::Size GetMinimumSize() const = 0;
+  virtual gfx::Size GteMaximumSize() const = 0;
+
+  virtual bool CanFocus() = 0;
+  virtual bool OnCaptureLost() = 0;
+  virtual void OnPaint(gfx::Canvas* canvas) = 0;
+private:
+  DISALLOW_COPY_AND_ASSIGN(WindowDelegate);
+};
 }  // namespace win
 }  // namespace azer
