64b4da415eee6c880a3e46d6b274e9895ac8de5c
lenxyang <lenxyang@gmail.com>
2015-01-14 16:11:29 +0800

    .

--- ui/compositor/demo/main.cc
+++ ui/compositor/demo/main.cc
@@ -1,15 +1,18 @@
 #include "base/message_loop/message_loop.h"
+#include "base/time/time.h"
 
+#include "azer/base/appinit.h"
 #include "azer/ui/win/context.h"
 #include "azer/ui/win/window_tree_host.h"
 #include "azer/render/util/render_window.h"
 
-class RenderFrame : public RenderLoop::Delegate {
+class RenderFrame : public azer::RenderLoop::Delegate {
  public:
   RenderFrame() {}
   ~RenderFrame() {}
 
-  bool Initialize(RenderLoop* renderer) override {
+  bool Initialize(azer::RenderLoop* renderer) override {
+    return true;
   }
   void OnUpdate(const ::base::Time& Time,
                 const ::base::TimeDelta& delta) override {
@@ -22,8 +25,9 @@ class RenderFrame : public RenderLoop::Delegate {
 };
 
 int main(int argc, char* argv[]) {
-  ::azer::InitApp(&argc, &argv);
-  azer::RenderWindow window;
-  window.Loop(delegate);
+  ::azer::InitApp(&argc, &argv, "");
+  RenderFrame delegate;
+  azer::util::RenderWindow window(gfx::Rect(100, 100, 800, 600));
+  window.Loop(&delegate);
   return 0;
 }
