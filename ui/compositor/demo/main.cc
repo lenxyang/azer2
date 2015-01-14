775f8cbf63d64d0dab2ba1cada74554f5901d289
lenxyang <lenxyang@gmail.com>
2015-01-14 16:25:55 +0800

    .

--- ui/compositor/demo/main.cc
+++ ui/compositor/demo/main.cc
@@ -19,6 +19,11 @@ class RenderFrame : public azer::RenderLoop::Delegate {
   }
   void OnRender(const ::base::Time& Time,
                 const ::base::TimeDelta& delta) override {
+    azer::RenderSystem* rs = azer::RenderSystem::Current();
+    azer::Renderer* renderer = rs->GetSwapchainRenderer();
+    renderer->Use();
+    renderer->Clear(azer::Vector4(0.0f, 0.0f, 1.0f, 1.0f));
+    renderer->ClearDepthAndStencil();
   }
  private:
   DISALLOW_COPY_AND_ASSIGN(RenderFrame);
