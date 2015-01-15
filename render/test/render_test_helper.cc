f7ba9b5d504f95e1a5675f01c2f2213b3fa0cdcc
lenxyang <lenxyang@gmail.com>
2015-01-15 12:01:37 +0800

    .

---
+++ render/test/render_test_helper.cc
@@ -0,0 +1,15 @@
+#include "azer/render/test/render_test_helper.h"
+
+#include "azer/render/render_system.h"
+
+namespace azer {
+RenderTestHelper::RenderTestHelper()
+    : render_system_(NULL) {
+}
+
+RenderTestHelper::~RenderTestHelper() {
+}
+
+void RenderTestHelper::RunOneFrame() {
+}
+}  // namespace azer
---
+++ render/test/render_test_helper.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "base/basictypes.h"
+#include "base/callback.h"
+#include "base/time/time.h"
+
+namespace azer {
+
+class RenderSystem;
+
+/**
+ * 此类帮助创建某人窗口，加载 RenderSystem
+ * 此后用户可以进行任意渲染或者销毁 RenderSystem
+ *
+ */
+class RenderTestHelper {
+ public:
+  RenderTestHelper();
+  ~RenderTestHelper();
+
+  void RunOneFrame();
+ private:
+  RenderSystem* render_system_;
+  DISALLOW_COPY_AND_ASSIGN(RenderTestHelper);
+};
+}  // namespace azer
