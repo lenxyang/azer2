40be3a105fc64f429a6281fe5955bdb169324972
lenxyang <lenxyang@gmail.com>
2015-01-15 17:44:25 +0800

    .

--- ui/widget/widget_delegate.h
+++ ui/widget/widget_delegate.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#include "base/compiler_specific.h"
+#include "ui/events/event_constants.h"
+#include "ui/events/event_handler.h"
+#include "ui/gfx/native_widget_types.h"
 #include "azer/base/export.h"
 
 namespace gfx {
@@ -9,9 +13,22 @@ class Canvas;
 namespace azer {
 namespace widget {
 
-class AZER_EXPORT WidgetDelegate {
+class AZER_EXPORT WidgetDelegate : public ::ui::EventHandler {
  public:
+  WidgetDelegate() {}
+  virtual ~WidgetDelegate() {}
+  virtual gfx::Size GetMinimumSize() const = 0;
+  virtual gfx::Size GteMaximumSize() const = 0;
+
+  virtual bool CanFocus() = 0;
+  virtual bool OnCaptureLost() = 0;
   virtual void OnPaint(gfx::Canvas* canvas) = 0;
+
+  // Returns true if event handling should descend into |child|. |location| is
+  // in terms of the Widget.
+  virtual bool ShouldDescendIntoChildForEventHandling(
+      Widget* child,
+      const gfx::Point& location) = 0;
 };
 
 }  // namespace widget
