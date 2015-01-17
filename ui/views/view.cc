f4b33e44d45fa254853b17df6fa24d5d8815c773
yanglei <lenxyang@gmail.com>
2015-01-17 17:31:56 +0800

    .

--- ui/views/view.cc
+++ ui/views/view.cc
@@ -1,5 +1,26 @@
 #include "azer/ui/views/view.h"
 
+#include "base/debug/trace_event.h"
+#include "base/logging.h"
+#include "base/memory/scoped_ptr.h"
+#include "base/message_loop/message_loop.h"
+#include "base/strings/stringprintf.h"
+#include "base/strings/utf_string_conversions.h"
+#include "third_party/skia/include/core/SkRect.h"
+#include "ui/gfx/canvas.h"
+#include "ui/gfx/interpolated_transform.h"
+#include "ui/gfx/path.h"
+#include "ui/gfx/point3_f.h"
+#include "ui/gfx/point_conversions.h"
+#include "ui/gfx/scoped_canvas.h"
+#include "ui/gfx/screen.h"
+#include "ui/gfx/skia_util.h"
+#include "ui/gfx/transform.h"
+
+#if defined(OS_WIN)
+#include "base/win/scoped_gdi_object.h"
+#endif
+
 
 namespace azer {
 namespace views {
@@ -9,5 +30,70 @@ View::View() {
 
 View::~View() {
 }
+
+bool View::Contains(const View* view) const {
+  for (const View* v = view; v; v = v->parent_) {
+    if (v == this)
+      return true;
+  }
+  return false;
+}
+
+void View::SetBounds(int x, int y, int width, int height) {
+}
+
+void View::SetBoundsRect(const gfx::Rect& bounds) {
+  
+}
+
+void View::SetSize(const gfx::Size& size) {
+  SetBounds(x(), y(), size.width(), size.height());
+}
+
+void View::SetPosition(const gfx::Point& position) {
+  SetBounds(position.x(), position.y(), width(), height());
+}
+
+void View::SetX(int x) {
+  SetBounds(x, y(), width(), height());
+}
+
+void View::SetY(int y) {
+  SetBounds(x(), y, width(), height());
+}
+
+gfx::View::Rect GetContentsBounds() const {
+}
+
+gfx::View::Rect GetLocalBounds() const {
+}
+
+gfx::View::Rect GetLayerBoundsInPixel() const {
+}
+
+gfx::View::Insets GetInsets() const {
+}
+
+void View::SetEnabled(bool enabled) {
+}
+
+void View::OnPaint(gfx::Canvas* canvas) {
+}
+
+void View::OnKeyEvent(ui::KeyEvent* event) {
+}
+
+void View::OnMouseEvent(ui::MouseEvent* event) {
+}
+
+void View::OnScrollEvent(ui::ScrollEvent* event) {
+}
+
+void View::OnTouchEvent(ui::TouchEvent* event) {
+}
+
+void View::OnGestureEvent(ui::GestureEvent* event) {
+}
+
 }  // namespace views
 }  // namespace azer
--- ui/views/view.h
+++ ui/views/view.h
@@ -1,7 +1,40 @@
 #pragma once
 
+#include <algorithm>
+#include <map>
+#include <set>
+#include <string>
+#include <vector>
+
+#include "base/compiler_specific.h"
+#include "base/i18n/rtl.h"
+#include "base/logging.h"
+#include "base/memory/scoped_ptr.h"
+#include "build/build_config.h"
+#include "ui/accessibility/ax_enums.h"
+#include "ui/base/accelerators/accelerator.h"
+#include "ui/base/dragdrop/drag_drop_types.h"
+#include "ui/base/dragdrop/drop_target_event.h"
+#include "ui/base/dragdrop/os_exchange_data.h"
+#include "ui/base/ui_base_types.h"
+#include "ui/events/event.h"
+#include "ui/events/event_target.h"
+#include "ui/gfx/geometry/r_tree.h"
+#include "ui/gfx/insets.h"
+#include "ui/gfx/native_widget_types.h"
+#include "ui/gfx/rect.h"
+#include "ui/gfx/vector2d.h"
+
 #include "azer/base/export.h"
 #include "azer/ui/widget/widget_delegate.h"
+
+namespace gfx {
+class Canvas;
+class Insets;
+class Path;
+class Transform;
+}
+
 
 namespace azer {
 
@@ -12,11 +45,76 @@ class Widget;
 namespace views {
 class AZER_EXPORT View : public widget::WidgetDelegate {
  public:
+  typedef std::vector<View> Views;
   View();
   ~View() override;
 
+  // Returns the parent view.
+  const View* parent() const { return parent_; }
+  View* parent() { return parent_; }
+
+  // Returns true if |view| is contained within this View's hierarchy, even as
+  // an indirect descendant. Will return true if child is also this view.
+  bool Contains(const View* view) const;
+
+  // Size and disposition ------------------------------------------------------
+  // Methods for obtaining and modifying the position and size of the view.
+  // Position is in the coordinate system of the view's parent.
+  // Position is NOT flipped for RTL. See "RTL positioning" for RTL-sensitive
+  // position accessors.
+  // Transformations are not applied on the size/position. For example, if
+  // bounds is (0, 0, 100, 100) and it is scaled by 0.5 along the X axis, the
+  // width will still be 100 (although when painted, it will be 50x50, painted
+  // at location (0, 0)).
+
+  void SetBounds(int x, int y, int width, int height);
+  void SetBoundsRect(const gfx::Rect& bounds);
+  void SetSize(const gfx::Size& size);
+  void SetPosition(const gfx::Point& position);
+  void SetX(int x);
+  void SetY(int y);
+
+  // No transformation is applied on the size or the locations.
+  const gfx::Rect& bounds() const { return bounds_; }
+  int x() const { return bounds_.x(); }
+  int y() const { return bounds_.y(); }
+  int width() const { return bounds_.width(); }
+  int height() const { return bounds_.height(); }
+  const gfx::Size& size() const { return bounds_.size(); }
+
+  // Returns the bounds of the content area of the view, i.e. the rectangle
+  // enclosed by the view's border.
+  gfx::Rect GetContentsBounds() const;
+
+  // Returns the bounds of the view in its own coordinates (i.e. position is
+  // 0, 0).
+  gfx::Rect GetLocalBounds() const;
+
+  // Returns the bounds of the layer in its own pixel coordinates.
+  gfx::Rect GetLayerBoundsInPixel() const;
+
+  // Returns the insets of the current border. If there is no border an empty
+  // insets is returned.
+  virtual gfx::Insets GetInsets() const;
+
+  // Set whether this view is enabled. A disabled view does not receive keyboard
+  // or mouse inputs. If |enabled| differs from the current value, SchedulePaint
+  // is invoked. Also, clears focus if the focused view is disabled.
+  void SetEnabled(bool enabled);
+
+  // Returns whether the view is enabled.
+  bool enabled() const { return enabled_; }
+ protected:
   void OnPaint(gfx::Canvas* canvas) override;
+
+  // Overridden from ui::EventHandler:
+  void OnKeyEvent(ui::KeyEvent* event) override;
+  void OnMouseEvent(ui::MouseEvent* event) override;
+  void OnScrollEvent(ui::ScrollEvent* event) override;
+  void OnTouchEvent(ui::TouchEvent* event) final;
+  void OnGestureEvent(ui::GestureEvent* event) override;
  private:
+  scoped_ptr<widget::Widget> widget_;
   DISALLOW_COPY_AND_ASSIGN(View);
 };
 }  // namespace views
