eb41b03413684c5e9f93f7d61c02bf3ecb8cbb81
lenxyang <lenxyang@gmail.com>
2015-01-20 08:54:23 +0800

    .

---
+++ ui/views/controls.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "azer/ui/views/view.h"
+#include "azer/ui/views/root_view.h"
+#include "azer/ui/views/controls/label.h"
--- ui/views/demo/main.cc
+++ ui/views/demo/main.cc
@@ -1,12 +1,10 @@
 
 #include "base/memory/scoped_ptr.h"
-#include "base/message_loop/message_loop.h"
 #include "azer/render/render.h"
 #include "azer/ui/base/environment.h"
 #include "azer/ui/widget/api.h"
 #include "azer/ui/compositor/api.h"
-#include "azer/ui/views/view.h"
-#include "azer/ui/views/root_view.h"
+#include "azer/ui/views/controls.h"
 
 using azer::compositor::Compositor;
 using azer::widget::WidgetTreeHost;
