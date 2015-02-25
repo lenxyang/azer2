5f460a6a623526ed6e886bd25a71ef86477d9bd9
lenxyang <lenxyang@gmail.com>
2015-02-25 16:42:10 +0800

    .

---
+++ ui/views/view_unittests.cc
@@ -0,0 +1,127 @@
+#include "base/memory/scoped_ptr.h"
+#include "base/rand_util.h"
+#include "base/strings/string_util.h"
+#include "base/strings/stringprintf.h"
+#include "base/strings/utf_string_conversions.h"
+#include "ui/base/accelerators/accelerator.h"
+#include "ui/base/clipboard/clipboard.h"
+#include "ui/base/l10n/l10n_util.h"
+#include "ui/events/event.h"
+#include "ui/events/keycodes/keyboard_codes.h"
+#include "ui/gfx/canvas.h"
+#include "ui/gfx/path.h"
+#include "ui/gfx/transform.h"
+#include "ui/strings/grit/ui_strings.h"
+
+#include "azer/ui/views/view.h"
+
+namespace views {
+
+typedef ViewsTestBase ViewTest;
+
+// A derived class for testing purpose.
+class TestView : public View {
+ public:
+  TestView()
+      : View(),
+        delete_on_pressed_(false),
+        native_theme_(NULL),
+        can_process_events_within_subtree_(true) {}
+  ~TestView() override {}
+
+  // Reset all test state
+  void Reset() {
+    did_change_bounds_ = false;
+    last_mouse_event_type_ = 0;
+    location_.SetPoint(0, 0);
+    received_mouse_enter_ = false;
+    received_mouse_exit_ = false;
+    last_clip_.setEmpty();
+    accelerator_count_map_.clear();
+    can_process_events_within_subtree_ = true;
+  }
+
+  // Exposed as public for testing.
+  void DoFocus() {
+    views::View::Focus();
+  }
+
+  void DoBlur() {
+    views::View::Blur();
+  }
+
+  bool focusable() const { return View::focusable(); }
+
+  void set_can_process_events_within_subtree(bool can_process) {
+    can_process_events_within_subtree_ = can_process;
+  }
+
+  bool CanProcessEventsWithinSubtree() const override {
+    return can_process_events_within_subtree_;
+  }
+
+  void OnBoundsChanged(const gfx::Rect& previous_bounds) override;
+  bool OnMousePressed(const ui::MouseEvent& event) override;
+  bool OnMouseDragged(const ui::MouseEvent& event) override;
+  void OnMouseReleased(const ui::MouseEvent& event) override;
+  void OnMouseEntered(const ui::MouseEvent& event) override;
+  void OnMouseExited(const ui::MouseEvent& event) override;
+
+  void Paint(gfx::Canvas* canvas, const CullSet& cull_set) override;
+  void SchedulePaintInRect(const gfx::Rect& rect) override;
+  bool AcceleratorPressed(const ui::Accelerator& accelerator) override;
+
+  void OnNativeThemeChanged(const ui::NativeTheme* native_theme) override;
+
+  // OnBoundsChanged.
+  bool did_change_bounds_;
+  gfx::Rect new_bounds_;
+
+  // MouseEvent.
+  int last_mouse_event_type_;
+  gfx::Point location_;
+  bool received_mouse_enter_;
+  bool received_mouse_exit_;
+  bool delete_on_pressed_;
+
+  // Painting.
+  std::vector<gfx::Rect> scheduled_paint_rects_;
+
+  // Painting.
+  SkRect last_clip_;
+
+  // Accelerators.
+  std::map<ui::Accelerator, int> accelerator_count_map_;
+
+  // Native theme.
+  const ui::NativeTheme* native_theme_;
+
+  // Value to return from CanProcessEventsWithinSubtree().
+  bool can_process_events_within_subtree_;
+};
+
+////////////////////////////////////////////////////////////////////////////////
+// OnBoundsChanged
+////////////////////////////////////////////////////////////////////////////////
+
+void TestView::OnBoundsChanged(const gfx::Rect& previous_bounds) {
+  did_change_bounds_ = true;
+  new_bounds_ = bounds();
+}
+
+TEST_F(ViewTest, OnBoundsChanged) {
+  TestView v;
+
+  gfx::Rect prev_rect(0, 0, 200, 200);
+  gfx::Rect new_rect(100, 100, 250, 250);
+
+  v.SetBoundsRect(prev_rect);
+  v.Reset();
+  v.SetBoundsRect(new_rect);
+
+  EXPECT_TRUE(v.did_change_bounds_);
+  EXPECT_EQ(v.new_bounds_, new_rect);
+  EXPECT_EQ(v.bounds(), new_rect);
+}
+
+}  // namespace views
