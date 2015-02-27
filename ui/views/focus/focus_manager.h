a3e9266f3031e079116c8bfef160c298cd01da0b
lenxyang <lenxyang@gmail.com>
2015-02-27 11:17:09 +0800

    .

---
+++ ui/views/focus/focus_manager.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "base/basictypes.h"
+#include "azer/ui/views/views_export.h"
+
+namespace views {
+class VIEWS_EXPORT FocusManager {
+ public:
+ private:
+  DISALLOW_COPY_AND_ASSIGN(FocusManager);
+};
+}  // namespace views
---
+++ ui/views/focus/focusable.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "base/basictypes.h"
+#include "azer/ui/views/views_export.h"
+
+namespace views {
+class VIEWS_EXPORT Focusable {
+ public:
+  Focusable();
+  virtual ~Focusable();
+
+  Focusable* GetNext();
+  const Focusable* GetNext() const;
+
+  void Add(Focusable* focusable);
+  void Remove(Focusable* focusable);
+ private:
+  Focusable* first_sibling_;
+  Focusable* next_sibling_;
+  Focusable* prev_sibling_;
+  int32 group_id_;
+  DISALLOW_COPY_AND_ASSIGN(Focusable);
+};
+}  // namespace views
