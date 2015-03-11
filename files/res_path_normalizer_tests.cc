8674672337f05c5a92d8a8a272ed9cf6f6300887
yanglei <lenxyang@gmail.com>
2015-03-11 22:58:31 +0800

    .

--- files/res_path_normalizer_tests.cc
+++ files/res_path_normalizer_tests.cc
@@ -17,11 +17,20 @@ TEST(ResPathNormalizer, Dot) {
 }
 
 TEST(ResPathNormalizer, ProtoPath) {
+  {
+    ResPathNormalizer normalizer(AZER_LITERAL("rpc://www:d"));
+    normalizer.Normalize();
+    ASSERT_TRUE(normalizer.success());
+    ASSERT_EQ(normalizer.component(), AZER_LITERAL("d"));
+  }
 }
 
 TEST(ResPathNormalizer, Failed) {
   StringType cases[] = {
     AZER_LITERAL("//a:b:c"),
+    AZER_LITERAL("//a:http://"),
+    AZER_LITERAL("/B/c/d"),
+    AZER_LITERAL("/B/c/d&"),
   };
 
   for (size_t i = 0; i < arraysize(cases); ++i) {
