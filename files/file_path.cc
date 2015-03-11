714aa0f2fce7deac15588294820ce25f512abbd2
lenxyang <lenxyang@gmail.com>
2015-03-11 10:20:54 +0800

    .

--- files/file_path.cc
+++ files/file_path.cc
@@ -2,14 +2,60 @@
 
 #include "base/logging.h"
 #include "base/files/file_util.h"
+#include "base/strings/string_util.h"
+#include "base/strings/string_split.h"
 #include "base/strings/utf_string_conversions.h"
 
 namespace azer {
+
 const CharType FilePath::kSeperator = FILE_PATH_LITERAL('/');
+const StringType FilePath::kSeperatorStr = FILE_PATH_LITERAL("/");
+const CharType FilePath::kComponentSeperator = FILE_PATH_LITERAL(':');
+const StringType FilePath::kComponentSeperatorStr = FILE_PATH_LITERAL(":");
 const StringType FilePath::kRootPath = FILE_PATH_LITERAL("//");
 
 FilePath::FilePath(const char* path) {
-  ConvertPath<StringType>(path, &path_);
+  ConvertPath<StringType>(path, &fullpath_);
+  OnFullpathChanged(this->fullpath());
+}
+
+FilePath::FilePath(const StringType& fullpath)
+    : fullpath_(fullpath) {
+  OnFullpathChanged(this->fullpath());
+}
+
+FilePath::FilePath(const FilePath& fullpath)
+    : fullpath_(fullpath.value()){
+  OnFullpathChanged(this->fullpath());
+}
+
+FilePath::FilePath(const StringType& path, const StringType& component) {
+  StringType str = path;
+  CHECK(!EndsWith(str, kSeperatorStr, true));
+  str.append(kComponentSeperatorStr);
+  str.append(component);
+  OnFullpathChanged(this->fullpath());
+}
+
+FilePath::FilePath(const FilePath& path, const StringType& component) {
+  StringType str = path.value();
+  CHECK(!EndsWith(str, kSeperatorStr, true));
+  str.append(kComponentSeperatorStr);
+  str.append(component);
+  OnFullpathChanged(this->fullpath());
+}
+
+void FilePath::OnFullpathChanged(const StringType& fullpath) {
+  std::vector<StringType> vec;
+  ::base::SplitString(fullpath, kComponentSeperator, &vec);
+  if (vec.size() > 0u) {
+    path_ = vec[0];
+  }
+  if (vec.size() > 1u) {
+    component_ = vec[1];
+  }
+
+  CHECK_LE(vec.size(), 2u);
 }
 
 FilePath FilePath::AppendCopy(const StringType& path) const {
@@ -19,12 +65,14 @@ FilePath FilePath::AppendCopy(const StringType& path) const {
 }
 
 void FilePath::Append(const StringType& str) {
-  if (path_.back() == kSeperator) {
-    path_.append(str);
+  if (fullpath_.back() == kSeperator) {
+    fullpath_.append(str);
   } else {
-    path_.push_back(kSeperator);
-    path_.append(str);
+    fullpath_.push_back(kSeperator);
+    fullpath_.append(str);
   }
+
+  OnFullpathChanged(this->fullpath());
 }
 
 bool SplitPackage(const StringType& full, StringType* path,
--- files/file_path.h
+++ files/file_path.h
@@ -5,14 +5,18 @@
 #include "azer/base/string.h"
 
 namespace azer {
+// FilePath's format 
+// '//.../.../../filename:component
 // FilePath's root is setting in FileSystem
 // for example, if the filesystem root is c:/mydir,
 // the azer::FileSystem "//a", for native files is c:/mydir/a
 class AZER_EXPORT FilePath {
  public:
   FilePath(const char* path);
-  FilePath(const StringType& path) : path_(path) {}
-  FilePath(const FilePath& path) : path_(path.value()) {}
+  FilePath(const StringType& fullpath);
+  FilePath(const FilePath& fullpath);
+  FilePath(const StringType& path, const StringType& component);
+  FilePath(const FilePath& path, const StringType& component);
 
   FilePath& operator = (const FilePath& path) {
     path_ = path.value();
@@ -20,19 +24,28 @@ class AZER_EXPORT FilePath {
   }
 
   const StringType& value() const { return path_;}
+
   FilePath AppendCopy(const StringType& path) const;
   void Append(const StringType& str);
   bool empty() const { return path_.empty();}
 
+  const StringType& fullpath() const { return fullpath_;}
   const StringType& path() const { return path_;}
+  const StringType& component() const { return component_;}
 
   static const StringType kRootPath;
+  static const StringType kSeperatorStr;
+  static const CharType kSeperator;
+  static const StringType kComponentSeperatorStr;
+  static const CharType kComponentSeperator;
  protected:
+  void OnFullpathChanged(const StringType& type);
+  StringType fullpath_;
   StringType path_;
   StringType component_;
-  static const CharType kSeperator;
+  
 };
 
 AZER_EXPORT bool SplitPackage(const StringType& full, StringType* path,
-                                StringType* package);
+                              StringType* package);
 }  // namespace azer
