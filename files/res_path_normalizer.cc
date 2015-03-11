93dededb942c324eb74c1c4a02fd1d7c3cebd397
yanglei <lenxyang@gmail.com>
2015-03-11 22:48:27 +0800

    .

--- files/res_path_normalizer.cc
+++ files/res_path_normalizer.cc
@@ -30,82 +30,98 @@ void ResPathNormalizer::Normalize() {
   t.set_options(StringTokenizer::RETURN_DELIMS);
   while (t.GetNext()) {
     const StringType& token = t.token();
+    State state = state();
     if (token == AZER_LITERAL("/")) {
-      if (state() == kStart) {
-        set_state(kRootSlashBegin);
+      switch (state) {
+        case kStart:
+          set_state(kRootSlashBegin);
         continue;
-      } else if (state() == kRootSlashBegin) {
-        set_state(kRootSlashEnd);
+        case kRootSlashBegin:
+          set_state(kRootSlashEnd);
         proto_ = AZER_LITERAL("//");
         continue;
-      } else if (state() == kString) {
+        case kString:
         set_state(kSlash1);
         continue;
-      } else if (state() == kSlash1) {
+        case kSlash1:
         set_state(kSlash2);
         continue;
-      } else if (state() == kComma) {
-        set_state(kProtoSlahBegin);
-        continue;
-      } else if (state() == kProtoSlahBegin) {
-        set_state(kProtoSlahEnd);
-        if (dirs_.size() > 1u || !proto_.empty()) {
-          SetErrorMsg("invalid proto path");
-          return;
-        } else {
-          proto_ = dirs_.back();
-          proto_.append(AZER_LITERAL("://"));
-          dirs_.clear();
-        }
-        continue;
+        case kComma:
+          set_state(kProtoSlahBegin);
+          continue;
+        case kProtoSlahBegin:
+          set_state(kProtoSlahEnd);
+          if (dirs_.size() > 1u || !proto_.empty()) {
+            SetErrorMsg("invalid proto path");
+            return;
+          } else {
+            proto_ = dirs_.back();
+            proto_.append(AZER_LITERAL("://"));
+            dirs_.clear();
+          }
+          continue;
+        default:
+          continue;
       }
     } else if (token == AZER_LITERAL(":")) {
       set_state(kComma);
       continue;
     } else if (token == AZER_LITERAL(".")) {
-      if (state() == kComma) {
-        StringType str = AZER_LITERAL(".");
-        set_state(kNameDot);
-        component_.append(AZER_LITERAL("."));
-      } else if (state() == kNameDot) {
-        component_.append(AZER_LITERAL("."));
-      } else if (state() == kProtoSlahEnd
-                 || state() == kSlash1
-                 || state() == kSlash2) {
-        
-      } else if (state() == kStart) {
-        set_state(kDot1);
-      } else if (state() == kDot1) {
-        set_state(kDot2);
-      } else if (state() == kDot2) {
-        set_state(kDot2);
+      switch (state) {
+        case kComma:
+          StringType str = AZER_LITERAL(".");
+          set_state(kNameDot);
+          component_.append(AZER_LITERAL("."));
+          continue;
+        case kNameDot:
+          component_.append(AZER_LITERAL("."));
+          continue;
+        case kProtoSlahEnd:
+        case kSlash1:
+        case kSlash2:
+          continue;
+        case kStart:
+          set_state(kDot1);
+          continue;
+        case kDot1:
+          set_state(kDot2);
+          continue;
+        case kDot2:
+          set_state(kDot2);
+          continue;
+        default:
+          CHECK(false);
       }
     } else {
-      if (state() == kStart || state() == kProtoSlahEnd
-          || state() == kSlash1 || state() == kSlash2) {
-          
-        dirs_.push_back(token);
-        set_state(kString);
-        continue;
-      } else if (state() == kDot1 || state() == kDot2) {
-        dirs_.back().append(token);
-        set_state(kString);
-        continue;
-      } else if (state() == kNameDot) { 
-        component_.append(token);
-        continue;
-      } else if (state() == kComma) {
-        set_state(kString);
-        if (component_.empty()) {
-          state_ = kSlash1;
-          component_ = token;
-          SetErrorMsg("multiple component specified.");
+      switch (state) {
+        case kStart:
+        case kProtoSlahEnd:
+        case kSlash1:
+        case kSlash2:
+        case kProtoSlahEnd:
+          dirs_.push_back(token);
+          set_state(kString);
           continue;
-        } else {
-          return;
-        }
-      } else {
-        CHECK(false);
+        case kDot1:
+        case kDot2:
+          dirs_.back().append(token);
+          set_state(kString);
+          continue;
+        case kNameDot:
+          component_.append(token);
+          continue;
+        case kComma:
+          set_state(kString);
+          if (component_.empty()) {
+            state_ = kSlash1;
+            component_ = token;
+            SetErrorMsg("multiple component specified.");
+            continue;
+          } else {
+            return;
+          }
+        default:
+          CHECK(false);
       }
     }
   }
