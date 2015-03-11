0e57d2c860074c45b3ac299d57bdb39f76655d81
lenxyang <lenxyang@gmail.com>
2015-03-11 13:14:31 +0800

    .

---
+++ files/res_path_normalizer_tests.cc
@@ -0,0 +1,8 @@
+#include "azer/files/res_path_normalizer.h"
+#include "azer/files/res_path.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace azer {
+TEST(ResPathNormalizer, Base) {
+}
+}  // namespace azer
