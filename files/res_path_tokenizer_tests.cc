824bf13a753e899db22290c291a7b4d5338356b2
lenxyang <lenxyang@gmail.com>
2015-03-12 10:01:52 +0800

    .

---
+++ files/res_path_tokenizer_tests.cc
@@ -0,0 +1,8 @@
+#include "base/basictypes.h"
+#include "azer/files/path_tokenizer.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace azer {
+TEST(ResPathNormalizer, Base) {
+}
+}  // namespace azer
