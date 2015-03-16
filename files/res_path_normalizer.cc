d66d31bccde0ad4edfbbd7fb56e686fec411312b
lenxyang <lenxyang@gmail.com>
2015-03-16 12:56:10 +0800

    .

--- files/res_path_normalizer.cc
+++ files/res_path_normalizer.cc
@@ -9,6 +9,7 @@
 #include "azer/files/res_path_tokenizer.h"
 
 namespace azer {
+
 ResPathNormalizer::ResPathNormalizer() {
 }
 
@@ -17,25 +18,32 @@ bool ResPathNormalizer::Normalize(ResPath* path) {
   StringType pathstr = path->fullpath();
   path->clear();
   ResPathTokenizer tokenizer(pathstr);
+  StringType component;
+  std::vector<StringType> vec;
   while (tokenizer.GetNext() != ResPathTokenizer::kSuccess) {
     const StringType& token = tokenizer.token();
     ResPathTokenizer::TokenType type = tokenizer.type();
     switch (type) {
       case ResPathTokenizer::kComponent:
-        if (path->component_.empty()) {
-          path->component_ = token;
-          path->fullpath_.append(token);
-        } else {
-        }
+        component = token;
+        break;
+      case ResPathTokenizer::kPrevDir:
+        if (!component.empty()) return false;
+        if (vec.empty()) return false;
+        if (vec.back() == FILE_PATH_LITERAL("//")) return false;
+        vec.resize(vec.size() - 1);
         break;
       case ResPathTokenizer::kDirSplitter:
-        path->fullpath_.append(FILE_PATH_LITERAL("/"));
-        break;
       case ResPathTokenizer::kCurrentDir:
-      case ResPathTokenizer::kPrevDir:
+        if (!component.empty()) return false;
+        break;
       case ResPathTokenizer::kRoot:
+        if (!component.empty()) return false;
+        vec.push_back(FILE_PATH_LITERAL("//"));
+        break;
       case ResPathTokenizer::kName:
-        path->fullpath_.append(token);
+        if (!component.empty()) return false;
+        vec.push_back(token);
         break;
     }
   }
