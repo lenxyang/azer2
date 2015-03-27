860b0c24c263dd27ae4a25b3b4bc2584f3b5dc1c
lenxyang <lenxyang@gmail.com>
2015-03-27 09:45:26 +0800

    .

---
+++ ui/theme/theme.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "base/basictypes.h"
+#include "ui/native_theme/native_theme.h"
+#include "azer/ui/theme/theme_export.h"
+
+namespace azer {
+class AZER_THEME_EXPORT Theme : public ui::NativeTheme {
+ public:
+  ~Theme() override;
+  static Theme* GetInstance();
+  static Theme* SetInstance(Theme* theme);
+ protected:
+  Theme();
+ private:
+  static Theme* instance_;
+  DISALLOW_COPY_AND_ASSIGN(Theme);
+};
+}  // namespace azer
