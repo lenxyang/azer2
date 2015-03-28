1a6513a0ddaf4d49d3ef1c8ec84d7d8769d5d4c2
lenxyang <lenxyang@gmail.com>
2015-03-28 13:42:55 +0800

    .

--- ui/theme/theme.h
+++ ui/theme/theme.h
@@ -2,10 +2,11 @@
 
 #include "base/basictypes.h"
 #include "ui/native_theme/native_theme.h"
+#include "ui/native_theme/fallback_theme.h"
 #include "azer/ui/theme/theme_export.h"
 
 namespace azer {
-class AZER_THEME_EXPORT Theme : public ui::NativeTheme {
+class AZER_THEME_EXPORT Theme : public ui::FallbackTheme {
  public:
   ~Theme() override;
   static Theme* GetInstance();
