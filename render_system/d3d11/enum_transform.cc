48e0197e3ebdf11535032479307e65fba4e6f304
lenxyang <lenxyang@gmail.com>
2015-07-16 17:40:41 +0800

    .

--- render_system/d3d11/enum_transform.cc
+++ render_system/d3d11/enum_transform.cc
@@ -35,6 +35,9 @@ DXGI_FORMAT TranslateFormat(DataFormat type) {
     case kRGBAn8: return DXGI_FORMAT_R8G8B8A8_UNORM;
     case kRGBAf: return DXGI_FORMAT_R32G32B32A32_FLOAT;
     case kBGRAn8: return DXGI_FORMAT_B8G8R8A8_UNORM;
+    case kRGBA8: NOTREACHED(); return DXGI_FORMAT_R8G8B8A8_UINT;
+    case kRGBA32: NOTREACHED(); return DXGI_FORMAT_R32G32B32A32_UINT;
+    case kRGBAn32: return DXGI_FORMAT_R32G32B32A32_FLOAT;
     case kDepth24Stencil8: return DXGI_FORMAT_D24_UNORM_S8_UINT;
     case kScalar: return DXGI_FORMAT_D32_FLOAT;
     case kVec2: return DXGI_FORMAT_R32G32_FLOAT;
@@ -52,10 +55,6 @@ DXGI_FORMAT TranslateFormat(DataFormat type) {
     case kBoolVec2: return DXGI_FORMAT_R32G32_SINT;
     case kBoolVec3: return DXGI_FORMAT_R32G32B32_SINT;
     case kBoolVec4: return DXGI_FORMAT_R32G32B32A32_SINT;
-    case kRGBA8: NOTREACHED(); return DXGI_FORMAT_R8G8B8A8_UINT;
-    case kRGBA32: NOTREACHED(); return DXGI_FORMAT_R32G32B32A32_UINT;
-      // no return DXGI_FORMAT_R32G32B32A32_UNORM;
-    case kRGBAn32: NOTREACHED(); return DXGI_FORMAT_R32G32B32A32_UINT;
     default:
       DCHECK(false) << "Unsupport type: " << (int32)type;
       return DXGI_FORMAT_UNKNOWN;
