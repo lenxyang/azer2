14e3cc06286ca8c72d8feb03841f80c69a1b199e
yanglei <lenxyang@gmail.com>
2015-09-29 07:51:05 +0800

    .

--- render/util.h
+++ render/util.h
@@ -1,10 +1,11 @@
 #pragma once
 
 #include "azer/math/math.h"
-#include "azer/render/indices_buffer.h"
+#include "azer/render/util/coordinate_grid.h"
 #include "azer/render/util/geometry/box_object.h"
 #include "azer/render/util/geometry/cone_object.h"
 #include "azer/render/util/geometry/sphere_object.h"
+#include "azer/render/util/light.h"
 #include "azer/render/util/pvw_effect.h"
 #include "azer/render/util/shader_util.h"
 #include "azer/render/util/tile.h"
