918096a56e5357efc6e4d779d8920766dd03315d
lenxyang <lenxyang@gmail.com>
2015-11-30 09:33:27 +0800

    refactory FileSystem

--- files/file_system.cc
+++ files/file_system.cc
@@ -5,6 +5,14 @@
 #include "azer/files/native_file_system.h"
 
 namespace azer {
+File::File(FileSystem* fs)
+    : file_system_(fs) {
+}
+
+FileSystem* File::file_system() { return file_system_;}
+const FileInfo& File::GetFileInfo() const { return file_info_;}
+FileType File::GetType() const { return file_info_.type();}
+int64 File::GetSize() const { return file_info_.size;}
 
 FileSystem::FileSystem() {
 }
--- files/file_system.h
+++ files/file_system.h
@@ -13,6 +13,43 @@
 #include "azer/files/file_content.h"
 
 namespace azer {
+enum FileType {
+  kDirectory,
+  kArchiveFile,
+  kSystemFile,
+};
+
+struct FileInfo {
+  base::FilePath path;
+  FileType type;
+  int64 size;
+};
+typedef std::vector<FileInfo> FileInfoVec;
+
+class AZER_EXPORT File : ::base::RefCountedThreadSafeBase<File> {
+ public:
+  explicit File(FileSystem* file_system);
+  virtual ~File();
+
+  FileSystem* file_system();
+  const FileInfo& GetFileInfo() const;
+  FileType GetType() const;
+  int64 GetSize() const;
+
+  virtual bool Open(const ResPath& path) = 0;
+  virtual void Close() = 0;
+  
+  // Load content async
+  // params: if size == -1, read the whole file
+  virtual bool PRead(int64 offset, int64 size, std::vector<uint8>* content) = 0;
+  virtual bool AsyncPRead(int64 offset, int64 size, std::vector<uint8>* content, 
+                          base::Closure* callback) = 0;
+ protected:
+  FileSystem* file_system_;
+  FileInfo file_info_;
+  DISALLOW_COPY_AND_ASSIGN(File);
+};
+
 class AZER_EXPORT FileSystem {
  public:
   FileSystem();
@@ -20,27 +57,12 @@ class AZER_EXPORT FileSystem {
 
   virtual const char* GetFileSystemName() const = 0;
 
-  enum FileType {
-    kDirectory,
-    kArchiveFile,
-    kSystemFile,
-  };
+  virtual FilePtr CreateFileHandle() = 0;
+  virtual FilePtr Open(const ResPath& path) = 0;
   virtual FileType GetFileType(const ResPath& path) = 0;
   virtual bool IsPathExists(const ResPath& path) = 0;
   virtual int64 GetFileSize(const ResPath& path) = 0;
-
-  struct FileInfo {
-    base::FilePath path;
-    FileType type;
-    int64 size;
-  };
-  typedef std::vector<FileInfo> FileInfoVec;
   virtual bool EnumDirectory(const ResPath& path, FileInfoVec* vec) = 0;
-
-  // Load content async
-  virtual bool ReadFile(const ResPath& path, std::vector<uint8>* content) = 0;
-  virtual void ReadFileAsync(const ResPath& path, std::vector<uint8>* content,
-                             base::Closure* callback) = 0;
  protected:
   DISALLOW_COPY_AND_ASSIGN(FileSystem);
 };
