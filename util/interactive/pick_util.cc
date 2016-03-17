d8245fa648fecb0fa3d54635564a4d476f77304b
lenxyang <lenxyang@gmail.com>
2016-03-17 09:25:50 +0800

    .

---
+++ util/interactive/pick_util.cc
@@ -0,0 +1,48 @@
+#include "azer/util/interactive/pick_util.h"
+
+namespace azer {
+Vector4 CalcWorldPosFromScreen(const gfx::Point& pt, const gfx::Size& size, 
+                               const Camera* camera) {
+  float x = 2 * (float)pt.x() / (float)size.width() - 1.0f;
+  float y = -2 * (float)pt.y() / (float)size.height() + 1.0f;
+  Vector4 pos(x, y, 0.0f, 1.0f);
+
+  const Matrix4& proj = camera->frustum().projection();
+  const Matrix4& view = camera->GetViewMatrix();
+  Matrix4 mat = std::move(view.InverseCopy() * proj.InverseCopy());
+  
+  pos = mat * pos;
+  pos /= pos.w;
+  return pos;
+}
+
+Ray GetPickingRay(const gfx::Point& pt, const gfx::Size& size,
+                  const Camera* camera) {
+  // another method
+  // reference 3D Game engine design(2nd edition) 8.4, Object Picking
+  Vector4 pos = Vector4(camera->holder().position(), 1.0f);
+  Vector4 pos2 = CalcWorldPosFromScreen(pt, size, camera);
+  Vector4 dir = pos - pos2;
+  Vector3 nd(dir.x, dir.y, dir.z);
+  nd.Normalize(); 
+  return Ray(Vector3(pos.x, pos.y, pos.z), nd);
+}
+
+bool PickingPlane(const azer::Ray& ray, const azer::Plane& plane, 
+                  azer::Vector3* pt) {
+  float dot = ray.directional().dot(plane.normal());
+  if (std::abs(dot) < 0.001) {
+    return false;
+  }
+
+  *pt = plane.intersect(ray);
+  return true;
+}
+
+void CalcNCDVec(const Vector3& p, const Camera& camera, Vector3* ndc) {
+  Vector4 v = camera.GetProjViewMatrix() * Vector4(*p, 1.0f);
+  ndc.x = v.x / v.w;
+  ndc.y = v.x / v.w;
+  ndc.z = v.x / v.w;
+}
+}  // namespace azer
--- util/interactive/pick_util.h
+++ util/interactive/pick_util.h
@@ -5,4 +5,28 @@
 #include "azer/math/vector4.h"
 
 namespace azer {
+class Camera;
+Vector4 CalcWorldPosFromScreen(const gfx::Point& pt, const gfx::Size& size, 
+                               const Camera* camera);
+
+/**
+ * Fucntion GetPickingRay
+ * 由屏幕坐标点及 Camera 信息获得一条射线
+ * params: pt 屏幕坐标点
+ *
+ */
+Ray GetPickingRay(const gfx::Point& pt, const gfx::Size& size,
+                  const Camera* camera);
+
+/**
+ * 获得射线与平面的交点
+ * return value: 如果有交点返回 true, 如果没有交点（平行）返回 false
+ */
+bool PickingPlane(const Ray& ray, const Plane& plane, Vector3* pt);
+
+/**
+ * 计算点在 NDC 空间内的位置
+ * 
+ */
+void CalcNCDVec(const Vector3& ps, const Camera& camera, Vector3* ndc);
 }  // namespace azer
