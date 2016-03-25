39f7889d3d31727eb95ff4ab0d6385375f53977c
lenxyang <lenxyang@gmail.com>
2016-03-25 13:54:25 +0800

    .

---
+++ util/interactive/spotlight_controller.cc
@@ -0,0 +1,69 @@
+#include "azer/util/interactive/spotlight_controller.h"
+
+#include "azer/effect/color_effect.h"
+#include "azer/render/camera.h"
+#include "azer/render/renderer.h"
+#include "azer/util/geometry/geometry.h"
+#include "azer/util/interactive/interactive_context.h"
+
+namespace azer {
+namespace {
+static const float kTopRadius = 0.5f;
+}
+SpotLightControllerObj::SpotLightControllerObj()
+    : theta_(),
+      phi_(),
+      range_(1.0f) {
+  InteractiveEnv* env = InteractiveEnv::GetInstance();
+  effect_ = (ColorEffect*)env->GetEffect("ColorEffect");
+  InitCircle();
+  InitBarrel();
+}
+
+SpotLightControllerObj::~SpotLightControllerObj() {}
+
+void SpotLightControllerObj::InitCircle() {
+}
+
+void SpotLightControllerObj::InitBarrel() {
+  Matrix4 mat = std::move(RotateX(Degree(-90.0f)));
+  float top_radius = kTopRadius;
+  float inner_sine = std::sqrt(1 - theta_ * theta_);
+  float inner_radius = range_ * inner_sine / theta_;
+  float outer_sine = std::sqrt(1 - phi_ * phi_);
+  float outer_radius = range_ * outer_sine / phi_;
+
+  GeoCylinderParam p;
+  
+}
+
+void SpotLightControllerObj::Update(const Camera* camera, const Vector3& position) {
+  effect_->SetPV(camera->GetProjViewMatrix());
+  effect_->SetWorld(Translate(position));
+}
+
+void SpotLightControllerObj::Render(Renderer* renderer) {
+  renderer->BindEffect(effect_);
+  entity_->Draw(renderer);
+}
+
+// class SpotLightController
+SpotLightController::SpotLightController(InteractiveContext* ctx)
+    : InteractiveController(ctx) {
+}
+
+SpotLightController::~SpotLightController() {
+}
+
+void SpotLightController::AddSpotLightObserver(SpotLightControllerObserver* obs) {
+  observer_list_.AddObserver(obs);
+}
+
+void SpotLightController::RemoteSpotLightObserver(SpotLightControllerObserver* obs) {
+  observer_list_.RemoveObserver(obs);
+}
+
+bool SpotLightController::HasSpotLightObserver(SpotLightControllerObserver* obs) {
+  return observer_list_.HasObserver(obs);
+}
+}  // namespace azer
---
+++ util/interactive/spotlight_controller.h
@@ -0,0 +1,75 @@
+#pragma once
+
+#include "base/basictypes.h"
+#include "azer/effect/color_effect.h"
+#include "azer/effect/light.h"
+#include "azer/math/quaternion.h" 
+#include "azer/math/vector3.h"
+#include "azer/math/vector4.h" 
+#include "azer/render/entity.h"
+#include "azer/util/interactive/interactive_controller.h"
+
+namespace azer {
+
+class Blending;
+class ColorEffect;
+typedef scoped_refptr<Blending> BlendingPtr;
+
+class SpotLightControllerObj {
+ public:
+  SpotLightControllerObj();
+
+  void ResetColor();
+  void Update(const Camera* camera, const Vector3& position);
+  void Render(Renderer* renderer);
+
+  void set_theta(float v) { theta_ = v;}
+  void set_phi(float v) { phi_ = v;}
+  void set_range(float v) { range_ = v;}
+  void set_inner_color(const Vector4& v} { inner_color_ = v;}
+  void set_outer_color(const Vector4& v} { outer_color_ = v;}
+ private:
+  void InitCircle();
+  void InitBarrel();
+  RasterizerStatePtr rasterizer_state_;
+  scoped_refptr<ColorEffect> color_effect_;
+  EntityPtr entity_;
+  BlendingPtr blending_;
+  float theta_;
+  float phi_;
+  float range_;
+  Vector4 inner_color_;
+  Vector4 outer_color_;
+  DISALLOW_COPY_AND_ASSIGN(SpotLightControllerObj);
+};
+
+class SpotLightController;
+class SpotLightControllerObserver {
+ public:
+  virtual void OnSpotLightPositionChanged(SpotLightController* controller) {}
+  virtual void OnSpotLightOrientationChanged(SpotLightController* controller) {}
+};
+
+class SpotLightController : public InteractiveController {
+ public:
+  explicit SpotLightController(InteractiveContext* ctx);
+  ~SpotLightController();
+
+  void SetLight(Light* ptr);
+
+  int32 GetPicking(const gfx::Point& pt) override;
+  void OnDragBegin(const gfx::Point& pt) override;
+  void OnDrag(const gfx::Point& pt) override;
+  void OnDragEnd(const gfx::Point& pt) override;
+  void UpdateFrame(const FrameArgs& args) override;
+  void RenderFrame(Renderer* renderer) override;
+
+  void AddSpotLightObserver(SpotLightControllerObserver* observer);
+  void RemoteSpotLightObserver(SpotLightControllerObserver* observer);
+  bool HasSpotLightObserver(SpotLightControllerObserver* observer);
+ private:
+  LightPtr light_;
+  ObserverList<SpotLightControllerObserver> observer_list_;
+  DISALLOW_COPY_AND_ASSIGN(SpotLightController);
+};
+}  // namespace azer
