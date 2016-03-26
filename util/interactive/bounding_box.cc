ae80b3261d01a10f495cc0d25521c460c946388a
lenxyang <lenxyang@gmail.com>
2016-03-26 15:43:58 +0800

    .

---
+++ util/interactive/bounding_box.cc
@@ -0,0 +1,50 @@
+#include "azer/util/interactive/bounding_box.h"
+
+#include "azer/effect/color_effect.h"
+#include "azer/math/math.h"
+#include "azer/render/camera.h"
+#include "azer/render/renderer.h"
+#include "azer/util/geometry/box.h"
+#include "azer/util/interactive/env.h"
+
+namespace azer {
+BoundingBoxObj::BoundingBoxObj() {
+  InteractiveEnv* env = InteractiveEnv::GetInstance();
+  color_effect_ = (ColorEffect*)env->GetEffect("ColorEffect");
+  box_ = new Entity(CreateBox(color_effect_->vertex_desc()));
+  boxframe_ = new Entity(CreateBoxFrame(color_effect_->vertex_desc()));
+}
+
+void BoundingBoxObj::SetMinAndMax(const Vector3& min, const Vector3& max) {
+  Vector3 position = (min + max) * 0.5f;
+  Vector3 scale(max.x - min.x, max.y - min.y, max.z - min.z);
+  world_ = std::move(Translate(position) * Scale(scale));
+}
+
+void BoundingBoxObj::Update(const Camera* camera) {
+  InteractiveEnv* env = InteractiveEnv::GetInstance();
+  color_effect_->SetPV(camera->GetProjViewMatrix());
+  color_effect_->SetWorld(world_);
+  color_effect_->SetLightData(&env->light()->data(), 1);
+}
+
+void BoundingBoxObj::Render(Renderer* renderer) {
+  InteractiveEnv* env = InteractiveEnv::GetInstance();
+  ScopedResetBlending scoped_blending(renderer);
+  renderer->SetBlending(env->blending(), 0, 0xffffffff);
+
+  ColorMaterialData mtrl;
+  mtrl.diffuse = color_;
+  mtrl.ambient = mtrl.diffuse * 0.4f;
+  mtrl.specular = mtrl.diffuse * 0.1f;
+  mtrl.alpha = 1.0f;
+  color_effect_->SetMaterial(mtrl);
+  renderer->BindEffect(color_effect_);
+  boxframe_->Draw(renderer);
+
+  mtrl.alpha = mtrl.diffuse.w;
+  color_effect_->SetMaterial(mtrl);
+  renderer->BindEffect(color_effect_);
+  box_->Draw(renderer);
+}
+}  // namespace azer
---
+++ util/interactive/bounding_box.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "base/memory/ref_counted.h"
+#include "azer/math/vector3.h"
+#include "azer/math/vector4.h"
+#include "azer/math/matrix4.h"
+
+namespace azer {
+
+class Camera;
+class ColorEffect;
+class Entity;
+class Renderer;
+
+class BoundingBoxObj {
+ public:
+  BoundingBoxObj();
+  
+  void SetColor(const Vector4& color) { color_ = color;}
+  void SetMinAndMax(const Vector3& min, const Vector3& max);
+  void Update(const Camera* camera);
+  void Render(Renderer* renderer);
+ private:
+  Vector3 min_, max_;
+  Vector4 color_;
+  Matrix4 world_;
+  scoped_refptr<Entity> box_;
+  scoped_refptr<Entity> boxframe_;
+  scoped_refptr<ColorEffect> color_effect_;
+  DISALLOW_COPY_AND_ASSIGN(BoundingBoxObj);
+};
+}  // namespace azer
