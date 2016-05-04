1afd875545cc655ced464fecb1fe896fa09a76c1
lenxyang <lenxyang@gmail.com>
2016-05-04 10:14:52 +0800

    .

---
+++ util/interactive/point_sprite.cc
@@ -0,0 +1,10 @@
+#include "azer/util/interactive/point_sprite.h"
+
+#include "azer/render/camera.h"
+#include "azer/render/renderer.h"
+
+namespace azer {
+PointSprite::PointSprite() {}
+void PointSprite::Render(const Camera& camera, Renderer* renderer) {
+}
+}  // namespace azer
