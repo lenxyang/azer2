7787ca2170f9e781903f48cf220f76e454a2a3d9
lenxyang <lenxyang@gmail.com>
2016-05-10 23:43:34 +0800

    .

---
+++ render/gpu_resource_view.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <memory>
+#include "base/basictypes.h"
+#include "base/memory/ref_counted.h"
+#include "azer/base/export.h"
+#include "azer/render/common.h"
+
+namespace azer {
+class AZER_EXPORT GpuResourceView : public ::base::RefCounted<GpuResourceView> {
+ public:
+ private:
+  DISALLOW_COPY_AND_ASSIGN(GpuResourceView);
+};
+}  // namespace azer
