92a59e00f9e16d73e98f25f4479ead2062286242
lenxyang <lenxyang@gmail.com>
2016-06-17 09:02:38 +0800

    .

--- util/interactive/graypoint.cc
+++ util/interactive/graypoint.cc
@@ -1,6 +1,7 @@
 #include "azer/util/interactive/graypoint.h"
 
 #include "azer/render/camera.h"
+#include "azer/effect/effectlib.h"
 #include "azer/util/geometry/arrow.h"
 #include "azer/util/interactive/env.h"
 #include "azer/util/interactive/orientation_util.h"
@@ -8,14 +9,20 @@
 namespace azer {
 GrayPoint::GrayPoint() 
     : scale_(1.0f, 1.0f, 1.0f) {
-  InteractiveEnv* env = InteractiveEnv::GetInstance();
-  color_ = Vector4(0.8f, 0.8f, 0.8f, 1.0f);
-  effect_ = (ColorEffect*)env->GetEffect("ColorEffect");
+  color_ = Vector4(1.0f, 1.0f, 1.0f, 1.0f);
+  effect_ = (TextBillboardEffect*)env->GetEffect("TextBillboardEffect");
   EntityDataPtr ptr(CreateArrow(effect_->vertex_desc()));
   arrow_ = new Entity(ptr.get());
   scale_ = Vector3(1.0f, 1.0f, 1.0f);
+
+  
 }
 
 GrayPoint::~GrayPoint() {
+}
+
+void GrayPoint::Render(const Camera& camera, Renderer* renderer) {
+  Matrix4 world_ = std::move(Translate(position_)) * std::move(Scale(scale_));
+  
 }
 }  // namespace azer
--- util/interactive/graypoint.h
+++ util/interactive/graypoint.h
@@ -13,10 +13,12 @@ class GrayPoint {
   void set_position(const Vector3& pos) { position_ = pos;}
   void set_scale(const Vector3& v) { scale_ = v;}
   void set_color(const Vector4& c) { color_ = c;}
+  void Render(const Camera& camera, Renderer* renderer);
  private:
   Vector3 position_;
   Vector3 scale_;
   Vector4 color_;
+  EntityPtr point_;
   scoped_refptr<TextBillboardEffect> effect_;
   DISALLOW_COPY_AND_ASSIGN(GrayPoint);
 };
