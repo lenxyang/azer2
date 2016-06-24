22808056323a200a5eaf916f373c13f0bf4b3f90
lenxyang <lenxyang@gmail.com>
2016-06-24 10:17:08 +0800

    .

--- ui/native_theme.cc
+++ ui/native_theme.cc
@@ -10,12 +10,32 @@ FallbackNativeTheme::~FallbackNativeTheme() {
 
 SkColor FallbackNativeTheme::GetSystemColor(ColorId color_id) const {
   switch (color_id) {
-    case kColorId_LabelEnabledColor:
-      return SkColorSetRGB(245, 245, 245);
-    case kColorId_LabelDisabledColor:
-      return SkColorSetRGB(149, 149, 149);
     case kColorId_LabelBackgroundColor:
       return SkColorSetRGB(40, 40, 40);
+    case kColorId_LabelDisabledColor:
+      return SkColorSetRGB(149, 149, 149);
+    case kColorId_LabelEnabledColor:
+      return SkColorSetRGB(245, 245, 245);
+    
+    case kColorId_ButtonEnabledColor:
+    case kColorId_TextOnCallToActionColor:
+    case kColorId_ResultsTableHoveredBackground:
+    case kColorId_ResultsTableSelectedBackground:
+    case kColorId_ResultsTableNormalUrl:
+    case kColorId_ResultsTableHoveredUrl:
+    case kColorId_ResultsTableSelectedUrl:
+      return SkColorSetRGB(245, 245, 245);
+    
+    case kColorId_ButtonBackgroundColor:
+      return SkColorSetRGB(40, 40, 40);
+    case kColorId_ButtonDisabledColor:
+      return SkColorSetRGB(149, 149, 149);
+    case kColorId_ButtonHighlightColor:
+      return SkColorSetRGB(245, 245, 245);
+    case kColorId_ButtonHoverColor:
+      return SkColorSetRGB(245, 245, 245);
+    case kColorId_ButtonHoverBackgroundColor:
+      return SkColorSetRGB(57, 57, 57);
     default:
       return NativeThemeAura::GetSystemColor(color_id);
   }
