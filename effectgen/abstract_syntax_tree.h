bc65005b82bce4dadd7e354d8f348709cd60727d
lenxyang <lenxyang@gmail.com>
2017-02-07 16:33:38 +0800

    remove effectgen

--- effectgen/abstract_syntax_tree.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-namespace azer {
-class AbstractSyntaxTree {
- public:
- private:
-  DISALLOW_COPY_AND_ASSIGN(AbstractSyntaxTree);
-};
-}  // namespace azer
--- render/texture.h
+++ render/texture.h
@@ -16,10 +16,8 @@ namespace azer {
 
 class RenderSystem;
 class ImageData;
-class SamplerState;
 class Texture;
 typedef scoped_refptr<Texture> TexturePtr;
-typedef scoped_refptr<SamplerState> SamplerStatePtr;
 
 enum class TexFormat {
   kUndefined = 0,
@@ -89,42 +87,6 @@ enum class TexFormat {
   kDXBC5n,
   kDXBC7n,
   kDXBC7nSRGB,
-};
-
-struct AZER_EXPORT SampleDesc {
-  int count;
-  int quality;
-
-  SampleDesc();
-};
-
-class AZER_EXPORT SamplerState : public ::base::RefCounted<SamplerState> {
- public:
-  struct AZER_EXPORT Options {
-    TexAddressMode wrap_u;
-    TexAddressMode wrap_v;
-    TexAddressMode wrap_w;
-    FilterMode mag_filter;
-    FilterMode min_filter;
-    FilterMode mip_filter;
-    CompareFunc compare_func;
-    FilterMode comp_mag_filter;
-    FilterMode comp_min_filter;
-    FilterMode comp_mip_filter;
-    Vector4 border_color;
-    int max_anisotropy;
- 
-    Options();
-  };
-
-  explicit SamplerState(const Options& state);
-  virtual ~SamplerState();
-  const Options& options() const { return options_;}
- protected:
-  virtual bool Init() = 0;
- private:
-  const Options options_;
-  DISALLOW_COPY_AND_ASSIGN(SamplerState);
 };
 
 struct AZER_EXPORT TexSize {
